8bc65ceb667ae461e0b97ce203df0ac5b4c399b0
Uygar Deniz ACAR <uygar.d.acar@gmail.com>
2024-09-07 23:24:34 +0300

    fixed overflow issue

--- frontend/src/components/MessageList.tsx
+++ frontend/src/components/MessageList.tsx
@@ -12,37 +12,36 @@ const MessageList = forwardRef<HTMLDivElement, MessageListProps>(
     const { id: currentUserId } = useUser();
 
     return (
-      <div className=' h-full px-4 overflow-y-auto flex flex-col-reverse'>
-        <div>
-          {messages?.pages.map((page, pageIndex) => (
-            <div key={pageIndex} className='flex flex-col-reverse h-full'>
-              {page.map((message) => (
+      <div className='  px-4 overflow-y-auto flex flex-col-reverse h-full pt-[0.1rem]'>
+        {messages?.pages.map((page, pageIndex) => (
+          <div key={pageIndex} className='flex flex-col-reverse '>
+            {page.map((message) => (
+              <div
+                key={message.id}
+                className={`flex  my-3 ${
+                  message?.authorId === currentUserId
+                    ? 'justify-end'
+                    : 'justify-start'
+                }`}
+              >
                 <div
-                  key={message.id}
-                  className={`flex my-4 ${
+                  className={`max-w-xs lg:max-w-md xl:max-w-lg px-4 py-2 rounded-lg ${
                     message?.authorId === currentUserId
-                      ? 'justify-end'
-                      : 'justify-start'
+                      ? 'bg-indigo-500 dark:bg-indigo-800 text-white'
+                      : 'bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200'
                   }`}
                 >
-                  <div
-                    className={`max-w-xs lg:max-w-md xl:max-w-lg px-4 py-2 rounded-lg ${
-                      message?.authorId === currentUserId
-                        ? 'bg-indigo-500 dark:bg-indigo-800 text-white'
-                        : 'bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200'
-                    }`}
-                  >
-                    <p>{message.text}</p>
-                    <p className='text-xs mt-1 opacity-75'>
-                      {new Date(message?.createdAt).toLocaleString()}
-                    </p>
-                  </div>
+                  <p>{message.text}</p>
+                  <p className='text-xs mt-1 opacity-75'>
+                    {new Date(message?.createdAt).toLocaleString()}
+                  </p>
                 </div>
-              ))}
-              <div ref={ref}></div>
-            </div>
-          ))}
-        </div>
+              </div>
+            ))}
+            <div ref={ref}></div>
+          </div>
+        ))}
+
         {(!messages ||
           messages.pages.length === 0 ||
           messages.pages[0].length === 0) && (
